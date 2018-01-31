f01ed635e8ed8cf3bdce1d9ff56cacefc4415a6a
Petter Remen <petter.remen@gmail.com>
2018-01-31 13:54:47 +0100

    commit

--- src/main/kotlin/io/github/remen/ktorjunit4/Application.kt
+++ src/main/kotlin/io/github/remen/ktorjunit4/Application.kt
@@ -3,12 +3,14 @@ package io.github.remen.ktorjunit4
 import io.github.remen.ktorjunit4.jackson.JacksonFeature
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.features.CallLogging
 import io.ktor.response.respond
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.ApplicationEngine
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import org.slf4j.event.Level
 import java.util.concurrent.TimeUnit
 
 class Application {
@@ -16,6 +18,9 @@ class Application {
     init {
         server = embeddedServer(Netty, 8080) {
             install(JacksonFeature)
+            install(CallLogging) {
+                level = Level.INFO
+            }
 
             routing {
                 get("/health") {
